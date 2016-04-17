2e97479d832049bdfc2827815439db32d648de08
Alex Perry <alexperry2@gmail.com>
2016-04-17 14:31:59 +0100

    Updated shipit file and adding circle file

--- shipitfile.js
+++ shipitfile.js
@@ -1,25 +1,29 @@
 var path = require('path');
 
 module.exports = function (shipit) {
-  require('shipit-deploy')(shipit);
+    require('shipit-deploy')(shipit);
 
-  shipit.initConfig({
-    default: {
-      workspace: '/tmp/eventcal-admin',
-      deployTo: '/home/eventcal-admin',
-      repositoryUrl: 'https://pezza3434@bitbucket.org/pezza3434/eventcal-admin.git',
-      ignores: ['.git'],
-      keepReleases: 5,
-      deleteOnRollback: false,
-      shallowClone: true
-    },
-    production: {
-      servers: 'root@horu.io'
-    }
-  });
-  shipit.on('published', function () {
-      shipit.remote('cd ' + shipit.releasePath + ' && npm install && npm run build').then(function(){
-        //   shipit.remote('pm2 restart horu');
-      });
-  });
+    shipit.initConfig({
+        default: {
+            workspace: '/tmp/eventcal-admin',
+            deployTo: '/home/eventcal-admin',
+            repositoryUrl: 'https://github.com/pezza3434/event-calendar-app.git',
+            ignores: ['.git'],
+            keepReleases: 5,
+            deleteOnRollback: false,
+            shallowClone: true
+        },
+        production: {
+            servers: 'root@horu.io'
+        }
+    });
+    shipit.on('updated', function () {
+        var buildDirectory = path.resolve('./public/build/');
+        shipit.remoteCopy(buildDirectory, shipit.releasePath);
+    });
+    shipit.on('published', function () {
+          shipit.remote('cd ' + shipit.releasePath + ' && npm install --production').then(function(){
+              shipit.remote('pm2 restart eventcal-admin');
+          });
+    });
 };
