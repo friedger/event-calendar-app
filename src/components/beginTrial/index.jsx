6636a1373a58b164fc12dc09159c40300d1dfce6
Alex Perry <alexperry2@gmail.com>
2016-05-06 17:10:28 +0100

    Small style adjustments

--- src/components/beginTrial/index.jsx
+++ src/components/beginTrial/index.jsx
@@ -1,3 +1,5 @@
+require('./style.scss');
+
 import React from 'react';
 const StripeCheckout = require('react-stripe-checkout');
 const config = require('../../../config');
@@ -8,7 +10,7 @@ export default React.createClass({
     },
     render() {
         return (
-            <div>
+            <div className="begin-trial">
                 <p><strong>This calendar can be integrated anywhere on your site with a simple line of code.</strong></p>
                 <p>Once you&#39;re ready to add your calendar to you&#39;re site, all new customers are entitled to a <strong>7 day trial</strong>. Only after that point, if you do not cancel, will your card be charged</p>
                 <StripeCheckout
