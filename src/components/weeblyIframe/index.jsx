2d8cab5f15cd44602e03ce17db9c6e10287f9e5f
Alex Perry <alexperry2@gmail.com>
2016-10-30 21:10:26 +0000

    Weebly iframe link use relative url

--- src/components/weeblyIframe/index.jsx
+++ src/components/weeblyIframe/index.jsx
@@ -3,12 +3,13 @@ if (typeof window !== 'undefined') {
 }
 
 import React from 'react';
+const config = require('../../../config');
 
 export default React.createClass({
     render() {
         return (
             <div className="weebly-iframe">
-                <a href="https://eventcalendarapp.hopto.org:1212/dashboard">Go to your Dashboard</a>
+                <a target="_blank" href="/dashboard">Go to your Dashboard</a>
             </div>
         )
     }
