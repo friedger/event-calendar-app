e08115e18c7925e1c0e8a8ad72d3f76b3d0a0d6e
Alex Perry <alexperry2@gmail.com>
2017-06-17 11:03:15 +0100

    Add new link page design

--- src/components/welcomePageHeader/index.jsx
+++ src/components/welcomePageHeader/index.jsx
@@ -4,7 +4,7 @@ if (typeof window !== 'undefined') {
 
 import React from 'react';
 import Collapse from 'react-collapse';
-import {Modal, Button} from 'react-bootstrap';
+import {Button, Modal} from 'react-bootstrap';
 
 import EventCal from '../eventCal';
 import CalendarSelection from '../calendarSelection';
@@ -14,11 +14,15 @@ import venobox from 'venobox/venobox/venobox.min.js';
 export default React.createClass({
     getInitialState() {
         return {
-            displayIcsForm: false
+            displayIcsForm: false,
+            showCronofyModal: false
         }
     },
     componentDidMount() {
         $('.venobox').venobox();
+    },
+    toggleModal() {
+        this.setState({showCronofyModal: !this.state.showCronofyModal});
     },
     render() {
 
@@ -26,52 +30,70 @@ export default React.createClass({
 
         return (
             <div className="welcome-page-header">
-                <h2>Get started with Event Calendar App</h2>
+                <h2>Link your calendar to Event Calendar App</h2>
                 <div className="welcome-page-header__sub-text">
-                <p>We connect to your existing calendar tools to create your <strong>new event calendar.</strong></p>
-                <p>How would you like to connect your existing calendar to Event Calendar App?</p>
+                <p>Where are your events, or where will they be in the future?</p>
                 </div>
                 <div className="row">
-                    <div className="col-md-4">
+                    <div className="col-md-12">
                         <div className="welcome-card welcome-card--primary">
-                            <div className="welcome-card__header welcome-card--primary__header">
-                                Option 1 (Cronofy*)
-                            </div>
-                            <div className="welcome-card__description welcome-card--primary__description">
-                                <p>The <strong>easiest</strong> way to link to your calendar.</p>
-                                <p>Works with <strong>Apple, Google</strong> and <strong>Outlook</strong> calendars.</p>
-                            </div>
-                            <div>
-                                <a href={this.props.authUrl} className="welcome-card__connect welcome-card--primary__connect">Connect</a>
-                            </div>
-                            <div className="welcome-card__cronofy-desription">*Cronofy is a third party service that we use to connect to your calendar. You will be briefly taken to another site when you click connect.</div>
-                        </div>
-                    </div>
-
-                    <div className="col-md-4">
-                        <div className="welcome-card">
-                            <div className="welcome-card__header">
-                                Option 2 (ICS Feed)
-                            </div>
-                            <div className="welcome-card__description">
-                                <p>Use for connecting to <strong>Facebook</strong> or other third party providers.</p>
-                                <p>Requires you to know the .ICS feed url of your calendar.</p>
-                            </div>
-                            <div>
-                                <a href="#" className="welcome-card__connect" onClick={this.props.clickIcsConnect}>Connect</a>
+                            <div className="row">
+                                <div className="col-sm-8">
+                                    <div className="welcome-card__header welcome-card--primary__header">
+                                        Google, Apple, Outlook or Exchange
+                                    </div>
+                                    <div className="welcome-card__description welcome-card--primary__description">
+                                        <p>The simplest way to connect your calendar to Event Calendar App</p>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4">
+                                    <div>
+                                        <a href='#' onClick={() => this.toggleModal()} className="welcome-card__connect welcome-card--primary__connect">Connect</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-md-12">
-                        <i className="fa fa-video-camera" aria-hidden="true"></i> <a className="venobox" data-autoplay="true" data-vbtype="video" href="https://www.youtube.com/embed/FCjGqazRmD0">If you like videos, we have made a short video fully explaining this step.</a>
-                        <h3>FAQ</h3>
-                            Dont have an existing calendar?
-                            <div className="welcome-page-header__collapse-content">
-                                <p>If you have a Google, Outlook or Apple account then you have a calendar. You may have just never used it. Otherwise, signing up for these services is completely free.</p>
-                                <p>You can read about why we require you to have a calendar, and how to get one yourself <a href="https://eventcalendarapp.com/support/2017/02/18/why-do-i-have-to-connect-my-calendar-to-event-calendar-app/">here.</a></p>
+                        <div className="welcome-card__recommended">
+                            <p>Most Popular!</p>
+                        </div>
+                    </div>
+
+                    <Modal show={this.state.showCronofyModal} onHide={this.toggleModal}>
+                        <Modal.Header closeButton>
+                            <Modal.Title><span>You're about to be taken to our partner...</span></Modal.Title>
+                          </Modal.Header>
+                        <div className="col-md-12 connection-modal">
+                            <div className="connection-modal__cronofy">
+                                <img src="https://3o7nze1htecu8vh6nuko0m13-wpengine.netdna-ssl.com/wp-content/themes/cronofy-2015/images/cronofy_logo-wide-colour.svg" />
                             </div>
-                        <div>
-                        <a target="_blank" href="https://eventcalendarapp.com/support/2017/02/18/ways-of-connecting-your-calendar/">Want more information about this step?</a>
+                            <div className="connection-modal__content">
+                                <p>We use a third party service called <strong>Cronofy</strong> to connect Event Calendar App to your Google, Apple, or Outlook calendar. You will briefly be taken away from our site while you connect.</p>
+                                <p>Don't worry, you only have to do this once!</p>
+                            </div>
+                            <a href={this.props.authUrl} className="action full-width">Connect</a>
+                        </div>
+                    </Modal>
+
+                    <div className="col-md-12">
+                        <div className="welcome-card">
+                            <div className="row">
+                                <div className="col-sm-8">
+                                    <div className="welcome-card__header">
+                                        ICS Feed
+                                    </div>
+                                    <div className="welcome-card__description">
+                                        <p>Use for connecting to <strong>Facebook</strong> or other third party providers.</p>
+                                        <p>Requires you to know the .ICS feed url of your calendar.</p>
+                                    </div>
+                                </div>
+                                <div className="col-sm-4">
+                                    <div>
+                                        <a href="#" className="welcome-card__connect" onClick={this.props.clickIcsConnect}>Connect</a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
