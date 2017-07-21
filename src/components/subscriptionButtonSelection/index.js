993c514516ce08185c4a289ddb9761c30da60272
Alex Perry <alexperry2@gmail.com>
2017-07-21 09:40:47 +0100

    Update how subscription button value is found

--- src/components/subscriptionButtonSelection/index.js
+++ src/components/subscriptionButtonSelection/index.js
@@ -2,17 +2,11 @@ import React from 'react';
 import {
     Row,
     Col,
-    Input,
-    Button,
-    HelpBlock,
     FormGroup,
     ControlLabel,
-    FormControl,
-    Radio,
-    Form
+    Radio
 } from 'react-bootstrap';
-import {reduxForm} from 'redux-form';
-import debounce from 'lodash.debounce';
+import { reduxForm } from 'redux-form';
 
 var Component = React.createClass({
     inputOnChange(e, field, handleSubmit) {
@@ -22,37 +16,68 @@ var Component = React.createClass({
         });
     },
     render() {
-        const {
-            fields: {
-                subscriptionButton
-            },
-            handleSubmit,
-            submitting
-        } = this.props;
+        const { fields: { subscriptionButton }, handleSubmit } = this.props;
         return (
             <Row className="settings-form">
                 <Col md={12}>
-                <form ref='settingsForm' className="form-horizontal">
-                    <FormGroup>
-                        <Row className="settings-space">
-                            <Col md={8}>
-                                <ControlLabel className="setting-title">Display subscription button:</ControlLabel>
-                            </Col>
-                            <Col md={4}>
-                                <Radio inline name="subscriptionButton" {...subscriptionButton} onChange={(e) => this.inputOnChange(e, subscriptionButton, handleSubmit)} checked={subscriptionButton.value === true || subscriptionButton.value === 'true'} value={true}>Yes</Radio>
-                                <Radio inline name="subscriptionButton" {...subscriptionButton} onChange={(e) => this.inputOnChange(e, subscriptionButton, handleSubmit)} checked={subscriptionButton.value === false || subscriptionButton.value === 'false'} value={false}>No</Radio>
-                            </Col>
-                        </Row>
-                    </FormGroup>
-                </form>
-            </Col>
+                    <form ref="settingsForm" className="form-horizontal">
+                        <FormGroup>
+                            <Row className="settings-space">
+                                <Col md={8}>
+                                    <ControlLabel className="setting-title">
+                                        Display subscription button:
+                                    </ControlLabel>
+                                </Col>
+                                <Col md={4}>
+                                    <Radio
+                                        inline
+                                        name="subscriptionButton"
+                                        {...subscriptionButton}
+                                        onChange={e =>
+                                            this.inputOnChange(e, subscriptionButton, handleSubmit)}
+                                        checked={
+                                            subscriptionButton.value === true ||
+                                            subscriptionButton.value === 'true'
+                                        }
+                                        value={true}
+                                    >
+                                        Yes
+                                    </Radio>
+                                    <Radio
+                                        inline
+                                        name="subscriptionButton"
+                                        {...subscriptionButton}
+                                        onChange={e =>
+                                            this.inputOnChange(e, subscriptionButton, handleSubmit)}
+                                        checked={
+                                            subscriptionButton.value === false ||
+                                            subscriptionButton.value === 'false'
+                                        }
+                                        value={false}
+                                    >
+                                        No
+                                    </Radio>
+                                </Col>
+                            </Row>
+                        </FormGroup>
+                    </form>
+                </Col>
             </Row>
-        )
+        );
     }
 });
 
-export default Component = reduxForm({ // <----- THIS IS THE IMPORTANT PART!
-    form: 'subscriptionButtonSelection', // a unique name for this form
-    fields: ['subscriptionButton'],
-    overwriteOnInitialValuesChange: false
-}, state => ({initialValues: state.appState}))(Component);
+export default (Component = reduxForm(
+    {
+        // <----- THIS IS THE IMPORTANT PART!
+        form: 'subscriptionButtonSelection', // a unique name for this form
+        fields: ['subscriptionButton'],
+        overwriteOnInitialValuesChange: false
+    },
+    state => {
+        const initialState = Object.assign({}, state.appState, {
+            subscriptionButton: state.appState.subscriptionButton.value
+        });
+        return { initialValues: initialState };
+    }
+)(Component));
