f497f72aa9d9a2518e8494c20dd57730cd0b5164
Alex Perry <alexperry2@gmail.com>
2017-07-21 11:31:20 +0100

    Show lock when user is not on a plan that can support subscriptions to calendars

--- src/components/subscriptionButtonSelection/index.js
+++ src/components/subscriptionButtonSelection/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {
-    Row,
-    Col,
-    FormGroup,
-    ControlLabel,
-    Radio
-} from 'react-bootstrap';
+import { Row, Col, FormGroup, ControlLabel, Radio } from 'react-bootstrap';
+import cn from 'classnames';
 import { reduxForm } from 'redux-form';
+import { Modal } from 'react-bootstrap';
 
 var Component = React.createClass({
+    getInitialState() {
+        return {
+            showModal: false
+        };
+    },
     inputOnChange(e, field, handleSubmit) {
         field.onChange(e);
         setTimeout(() => {
@@ -16,47 +17,91 @@ var Component = React.createClass({
         });
     },
     render() {
-        const { fields: { subscriptionButton }, handleSubmit } = this.props;
+        const { fields: { subscriptionButton }, handleSubmit, validWithPlan } = this.props;
         return (
-            <Row className="settings-form">
+            <Row
+                onClick={() => {
+                    if (!validWithPlan) {
+                        this.setState({ showModal: true });
+                    }
+                }}
+                className={cn('settings-form', { 'settings-form--invalid': !validWithPlan })}
+            >
+                <Modal
+                    show={this.state.showModal}
+                    onHide={() => {
+                        this.setState({ showModal: false });
+                    }}
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>
+                            <span>Enable users to subscribe to your calendar</span>
+                        </Modal.Title>
+                    </Modal.Header>
+                    <div className="col-md-12 connection-modal">
+                        <div className="connection-modal__content">
+                            Unfortunately this feature is not available on your current plan.
+                            Upgrade to enable it.
+                        </div>
+                    </div>
+                </Modal>
                 <Col md={12}>
                     <form ref="settingsForm" className="form-horizontal">
                         <FormGroup>
                             <Row className="settings-space">
                                 <Col md={8}>
-                                    <ControlLabel className="setting-title">
+                                    <ControlLabel
+                                        className={cn('setting-title', {
+                                            'setting-title--strike': !validWithPlan
+                                        })}
+                                    >
                                         Display subscription button:
                                     </ControlLabel>
                                 </Col>
+                                {validWithPlan &&
+                                    <Col md={4}>
+                                        <Radio
+                                            inline
+                                            name="subscriptionButton"
+                                            {...subscriptionButton}
+                                            onChange={e =>
+                                                this.inputOnChange(
+                                                    e,
+                                                    subscriptionButton,
+                                                    handleSubmit
+                                                )}
+                                            checked={
+                                                subscriptionButton.value === true ||
+                                                subscriptionButton.value === 'true'
+                                            }
+                                            value={true}
+                                        >
+                                            Yes
+                                        </Radio>
+                                        <Radio
+                                            inline
+                                            name="subscriptionButton"
+                                            {...subscriptionButton}
+                                            onChange={e =>
+                                                this.inputOnChange(
+                                                    e,
+                                                    subscriptionButton,
+                                                    handleSubmit
+                                                )}
+                                            checked={
+                                                subscriptionButton.value === false ||
+                                                subscriptionButton.value === 'false'
+                                            }
+                                            value={false}
+                                        >
+                                            No
+                                        </Radio>
+                                    </Col>}
                                 <Col md={4}>
-                                    <Radio
-                                        inline
-                                        name="subscriptionButton"
-                                        {...subscriptionButton}
-                                        onChange={e =>
-                                            this.inputOnChange(e, subscriptionButton, handleSubmit)}
-                                        checked={
-                                            subscriptionButton.value === true ||
-                                            subscriptionButton.value === 'true'
-                                        }
-                                        value={true}
-                                    >
-                                        Yes
-                                    </Radio>
-                                    <Radio
-                                        inline
-                                        name="subscriptionButton"
-                                        {...subscriptionButton}
-                                        onChange={e =>
-                                            this.inputOnChange(e, subscriptionButton, handleSubmit)}
-                                        checked={
-                                            subscriptionButton.value === false ||
-                                            subscriptionButton.value === 'false'
-                                        }
-                                        value={false}
-                                    >
-                                        No
-                                    </Radio>
+                                    <div className="settings-form__locked">
+                                        {!validWithPlan &&
+                                            <i className="fa fa-lock" aria-hidden="true" />}
+                                    </div>
                                 </Col>
                             </Row>
                         </FormGroup>
--- src/containers/adminSettingsPanel.jsx
+++ src/containers/adminSettingsPanel.jsx
@@ -4,30 +4,32 @@ import { bindActionCreators } from 'redux';
 import * as appActions from '../actions/index';
 import * as calendarActions from '../actions/calendarActions';
 
-import Header from '../components/header';
-
 import NumberOfEventsToDisplay from '../components/numberOfEventsSelection';
 import TimezoneSelection from '../components/timezoneSelection';
 import CalendarSelection from '../components/calendarSelection';
 import ViewModeSelection from '../components/viewModeSelection';
 import SubscriptionButtonSelection from '../components/subscriptionButtonSelection';
 import EmbedCode from '../components/embedCode';
+import get from 'lodash.get';
+import featurePermissions from '../utils/featurePermissions';
 
 import CalendarConnections from '../components/calendarConnections';
 
-const mapState = ({loginState, appState}) => {
+const mapState = ({ appState }) => {
     return {
         appState
-    }
-}
+    };
+};
 
-const mapDispatch = (dispatch) => {
-    return bindActionCreators({
-        ...appActions,
-        ...calendarActions
-    }, dispatch);
-}
-
+const mapDispatch = dispatch => {
+    return bindActionCreators(
+        {
+            ...appActions,
+            ...calendarActions
+        },
+        dispatch
+    );
+};
 
 const component = React.createClass({
     contextTypes: {
@@ -37,50 +39,79 @@ const component = React.createClass({
         return {
             displayConnectionsScreen: false,
             addCalendarSelected: false
-        }
+        };
     },
     toggleConnectionsScreen() {
         this.props.getCalendars(this.props.eventCalWidgetUuid);
-        this.setState({displayConnectionsScreen: !this.state.displayConnectionsScreen});
+        this.setState({ displayConnectionsScreen: !this.state.displayConnectionsScreen });
     },
-    _getCalendarFormInitialValues() {
+    getCalendarFormInitialValues() {
         return Object.keys(this.props.appState.calendars).reduce((collection, current) => {
-            //this is breaking - why?
+            // this is breaking - why?
             collection[current] = this.props.appState.calendars[current].selected;
             return collection;
         }, {});
     },
     render() {
-        return (
-            this.state.displayConnectionsScreen ?
-            <CalendarConnections
-                toggleConnectionsScreen={this.toggleConnectionsScreen}
-                connections={this.props.appState.connections}
-                calendarAdded={() => {this.props.getConnections()}}
-                deleteCalendar={this.props.deleteCalendar}
-                eventCalWidgetUuid={this.props.eventCalWidgetUuid}
-                />
-            :
-            <div className="dashboard-settings">
-                <div className="dashboard-header dashboard-header--right row">
-                    <div className="col-md-12">
-                        <span>Calendar settings</span>
-                        {!this.props.appState.user.weeblyUser &&<EmbedCode eventCalWidgetUuid={this.props.eventCalWidgetUuid} userIsAGuest={this.props.appState.user.status === 'registered'} userId={this.props.appState.user.userId} shopifyUser={this.props.appState.user.shopifyUser} calendarBuildUrl={this.props.calendarBuildUrl}></EmbedCode>}
-                    </div>
-                </div>
-                <CalendarSelection onChange={this.props.putCalendars.bind(null, this.props.eventCalWidgetUuid)}
-                    toggleConnectionsScreen={this.toggleConnectionsScreen}
-                    initialValues={this._getCalendarFormInitialValues()}
-                    loading={this.props.appState.calendarsLoading}
-                    fields={Object.keys(this.props.appState.calendars)}
-                    calendars={this.props.appState.calendars}/>
-                <ViewModeSelection putSettingsAction={this.props.putSettings.bind(null, this.props.eventCalWidgetUuid)}/>
-                <NumberOfEventsToDisplay putSettingsAction={this.props.putSettings.bind(null, this.props.eventCalWidgetUuid)}/>
-                <TimezoneSelection putSettingsAction={this.props.putSettings.bind(null, this.props.eventCalWidgetUuid)}/>
-                <SubscriptionButtonSelection putSettingsAction={this.props.putSettings.bind(null, this.props.eventCalWidgetUuid)}/>
-            </div>
-        )
+        return this.state.displayConnectionsScreen
+            ? <CalendarConnections
+                  toggleConnectionsScreen={this.toggleConnectionsScreen}
+                  connections={this.props.appState.connections}
+                  calendarAdded={() => {
+                      this.props.getConnections();
+                  }}
+                  deleteCalendar={this.props.deleteCalendar}
+                  eventCalWidgetUuid={this.props.eventCalWidgetUuid}
+              />
+            : <div className="dashboard-settings">
+                  <div className="dashboard-header dashboard-header--right row">
+                      <div className="col-md-12">
+                          <span>Calendar settings</span>
+                          {!this.props.appState.user.weeblyUser &&
+                              <EmbedCode
+                                  eventCalWidgetUuid={this.props.eventCalWidgetUuid}
+                                  userIsAGuest={this.props.appState.user.status === 'registered'}
+                                  userId={this.props.appState.user.userId}
+                                  shopifyUser={this.props.appState.user.shopifyUser}
+                                  calendarBuildUrl={this.props.calendarBuildUrl}
+                              />}
+                      </div>
+                  </div>
+                  <CalendarSelection
+                      onChange={this.props.putCalendars.bind(null, this.props.eventCalWidgetUuid)}
+                      toggleConnectionsScreen={this.toggleConnectionsScreen}
+                      initialValues={this.getCalendarFormInitialValues()}
+                      loading={this.props.appState.calendarsLoading}
+                      fields={Object.keys(this.props.appState.calendars)}
+                      calendars={this.props.appState.calendars}
+                  />
+                  <ViewModeSelection
+                      putSettingsAction={this.props.putSettings.bind(
+                          null,
+                          this.props.eventCalWidgetUuid
+                      )}
+                  />
+                  <NumberOfEventsToDisplay
+                      putSettingsAction={this.props.putSettings.bind(
+                          null,
+                          this.props.eventCalWidgetUuid
+                      )}
+                  />
+                  <TimezoneSelection
+                      putSettingsAction={this.props.putSettings.bind(
+                          null,
+                          this.props.eventCalWidgetUuid
+                      )}
+                  />
+                  <SubscriptionButtonSelection
+                      validWithPlan={get(this, 'props.appState.user.status') && featurePermissions.checkFeatureAvailability(this.props.appState.user.status, 'subscriptions')}
+                      putSettingsAction={this.props.putSettings.bind(
+                          null,
+                          this.props.eventCalWidgetUuid
+                      )}
+                  />
+              </div>;
     }
 });
 
-export default connect(mapState, mapDispatch)(component)
+export default connect(mapState, mapDispatch)(component);
