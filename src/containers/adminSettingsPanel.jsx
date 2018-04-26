fff51d43f2d06d68acc038300099527dff7bfaa1
Alex Perry <alexperry2@gmail.com>
2018-04-26 15:02:22 +0800

    Add styling for when event is being edited

--- src/containers/adminSettingsPanel.jsx
+++ src/containers/adminSettingsPanel.jsx
@@ -136,8 +136,22 @@ const component = React.createClass({
         return false;
     },
     render() {
+        const content = "'Editing this event'";
         return (
             <div className="dashboard-settings">
+                {this.eventActivated() &&
+                    <style>
+                        {`
+                            #event-calendar-app .calendar-list-view .calendar-list-event.uuid-${this.props.eventState.uuid}.col-md-12{
+                                border: 3px solid #da4167 !important;
+                                border-bottom: 3px solid #da4167 !important;
+                            }
+                            #event-calendar-app .calendar-list-event.uuid-${this.props.eventState.uuid}:after{
+                                display: block;
+                            }
+                        `}
+                    </style>
+                }
                 {/* Onboading modals*/}
                 <AddedFirstEventSuccess
                     show={this.state.showFirstEventSuccessModal}
