b07eac94166125d1205791f10afb396db9a454dc
Alex Perry <alexperry2@gmail.com>
2018-08-03 12:48:52 +0100

    Ensure embed code message and no events message don't overlap

--- src/components/editor/noEventsMessage/index.jsx
+++ src/components/editor/noEventsMessage/index.jsx
@@ -8,11 +8,13 @@ export default React.createClass({
     },
     render() {
         return (
-            <div className="eca-dashboard__no-events-message-container">
+            <div className="row">
+                <div className="eca-dashboard__no-events-message-container">
                     <a className="eca-dashboard__no-events-message venobox" data-gall="gall-video" data-autoplay="true" data-vbtype="video" href="https://www.youtube.com/watch?v=B8EpN74FVBE">
                         <p><i className="fa fa-plus" aria-hidden="true"></i> Add some events to your selected calendars to get started</p>
                         <p>Need some help?</p>
                     </a>
+                </div>
             </div>
         )
     }
