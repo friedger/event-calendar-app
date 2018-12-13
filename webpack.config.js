ebc1a37f65b0d36249e4672a521116aa0fd306ef
Alex Perry <alexperry2@gmail.com>
2018-12-13 12:25:28 +0000

    Allow babel to transpile query string

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
             },
             {
                 test: /\.jsx?$/,
-                exclude: /(node_modules)/,
+                exclude: /node_modules\/(?!(query-string|strict-uri-encode)\/).*/,
                 loader: 'babel',
                 query: {
                     presets: ['es2015', 'react'],
--- webpack.dist.js
+++ webpack.dist.js
@@ -14,7 +14,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.jsx?$/,
-                exclude: /(node_modules)/,
+                exclude: /node_modules\/(?!(query-string|strict-uri-encode)\/).*/,
                 loader: 'babel',
                 query: {
                     presets: ['es2015', 'react']
